e55304fbd775faee69158b13d1a98ea43a87f90c
Anceag <maksimkorokin1@gmail.com>
2019-06-18 21:25:21 +0300

    Device

--- StocksDevice/Form1.Designer.cs
+++ StocksDevice/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace StocksDevice
 {
-    partial class Form1
+    partial class device
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.deviceIdTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.massTextBox = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(52, 137);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(100, 23);
+            this.sendBtn.TabIndex = 0;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.SendState);
+            // 
+            // deviceIdTextBox
+            // 
+            this.deviceIdTextBox.Location = new System.Drawing.Point(52, 25);
+            this.deviceIdTextBox.Name = "deviceIdTextBox";
+            this.deviceIdTextBox.Size = new System.Drawing.Size(100, 20);
+            this.deviceIdTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Device id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mass";
+            // 
+            // massTextBox
+            // 
+            this.massTextBox.Location = new System.Drawing.Point(52, 79);
+            this.massTextBox.Name = "massTextBox";
+            this.massTextBox.Size = new System.Drawing.Size(100, 20);
+            this.massTextBox.TabIndex = 4;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(52, 106);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(35, 13);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.Text = "label3";
+            // 
+            // device
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(203, 172);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.massTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deviceIdTextBox);
+            this.Controls.Add(this.sendBtn);
+            this.Name = "device";
+            this.Text = "Device";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.TextBox deviceIdTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox massTextBox;
+        private System.Windows.Forms.Label outputLabel;
     }
 }
 
--- StocksDevice/Form1.cs
+++ StocksDevice/Form1.cs
@@ -3,18 +3,61 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace StocksDevice
 {
-    public partial class Form1 : Form
+    public partial class device : Form
     {
-        public Form1()
+        public device()
         {
             InitializeComponent();
+            outputLabel.Text = "";
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+        }
+
+        private async void SendState(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://anceag-001-site1.itempurl.com/api/items/writestatewithdevice");
+                request.Method = "POST";
+                request.AutomaticDecompression = DecompressionMethods.GZip;
+                request.ContentType = "application/json";
+
+                using (var stream = await request.GetRequestStreamAsync())
+                using (var streamWriter = new StreamWriter(stream))
+                {
+                    if (int.TryParse(massTextBox.Text, out int mass) && int.TryParse(deviceIdTextBox.Text, out int id))
+                    {
+                        var state = new State() { DeviceId = id, Mass = mass };
+                        streamWriter.Write(JsonConvert.SerializeObject(state));
+                    }
+                    else
+                    {
+                        outputLabel.Text = "Data is invalid";
+                        return;
+                    }
+                }
+
+                using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    await reader.ReadToEndAsync();
+                    outputLabel.Text = "Success";
+                }
+            }
+            catch
+            {
+                outputLabel.Text = "Error";
+            }
         }
     }
 }
